2080bc3aa63bc413a80d92aaec09f42c083973db
gsb1776 <gray_bittker@brown.edu>
2025-03-09 17:59:00 -0400

    increased request num

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ reddit = praw.Reddit(
 )
 
 # List target subreddits
-subreddits = ["fantasyfootball", "nfl", "patriots"]
+subreddits = ["politics", "news", "socialism"]
 subreddits = [item.lower() for item in subreddits]
 
 # Set some parameters
 time_limit = "month"
-count = 50
+count = 30
 
 # Subreddit info will be stored as a dataframe with:
 # top_subs, a dict with ["subreddit": karma]
@@ -37,50 +37,26 @@ for sub in subreddits:
     authors = []
     for submission in subreddit.top(time_filter = time_limit, limit=count):
         authors.append(submission.author)
-    # Iterate through authors, getting their top subreddits and their karma 
+    # Iterate through authors, getting their top subreddits and their karma
     top_subs = {}
     for author in authors:
         if author != None:
-            # Get the top subreddits - COULD BE BLOCKED, need a check for this
-            # Need to change to PRAW due to API blocking
-            url = "https://www.reddit.com/user/" + author.name + "/top_karma_subreddits.json"
-            response = requests.get(url, headers = {'User-agent': 'your bot 0.1'})
-            # Retry until we get requests working, prevent overloading server
-            mult = 1
-            while response.status_code == 429:
-                print(mult)
-                time.sleep(2 ** mult)
-                response = requests.get(url, headers = {'User-agent': 'your bot 0.1'})
-                # Square mult
-                mult += 1 
-            # Load data as json
-            print(response)
-            data = json.loads(response.text)
-            # Iterate through all communities and add tuple
-            if data is not None and data != {}:
-                # Iterate by community
-                for community in data["data"]:
-                    # Avoid double counting
-                    if community["display_name"].lower() != sub:
-                        #print(community["display_name"])
-                        #print(community)
-                        name = community["display_name"].lower()
-                        karma = community["link_karma"] + community["comment_karma"]
-                        if name in top_subs:
-                            # Update stats
-                            top_subs[name] += karma
-                        else:
-                            # Or create entry
-                            top_subs[name] = (karma)
+            # Use praw to get the content of this user - avoids requests issue
+            content = []
+            # DESIGN QUESTION: Do we want top or most recent posts/comments
+            # Add comments and posts
+            content.extend(reddit.redditor(author.name).comments.top(limit = 10))
+            content.extend(reddit.redditor(author.name).submissions.top(limit = 10))
+            # Get top/newwest comments
+            for item in content:
+                # Add score
+                if item.subreddit in top_subs:
+                    top_subs[item.subreddit] += item.score
+                else:
+                    top_subs[item.subreddit] = item.score
     print(top_subs)
-    # sort it
-    top_subs = {k: v for k, v in sorted(top_subs.items(), key=lambda item: item[1], reverse=True)}
-    # Get the user count for the subreddit
-    url = "https://www.reddit.com/r/" + sub + "/about.json"
-    time.sleep(3)
-    response = requests.get(url, headers = {'User-agent': 'your bot 0.1'})
-    data = json.loads(response.text)["data"]
-    new_row = (sub, top_subs, data["subscribers"])
+    sortable = dict(sorted(top_subs.items(), key=lambda item: item[1], reverse=True))
+    new_row = (sub, sortable, subreddit.subscribers)
     subs.loc[len(subs)] = new_row
 print(subs)
 # Save
