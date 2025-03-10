3fc4eb8472491989bc2e8b7e5f7fa8937a26deec
gsb1776 <gray_bittker@brown.edu>
2025-03-09 21:27:06 -0400

    more reliable authentication waiting

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import pandas as pd
 import requests
 import time
 import json
+import time
+from datetime import datetime
 
 reddit = praw.Reddit(
     client_id="q33sziEQUMFF51P9IrtRHw",
@@ -20,7 +22,8 @@ subreddits = [item.lower() for item in subreddits]
 
 # Set some parameters
 time_limit = "month"
-count = 30
+# Number of posts to scan per subreddit
+count = 10
 
 # Subreddit info will be stored as a dataframe with:
 # top_subs, a dict with ["subreddit": karma]
@@ -28,18 +31,41 @@ count = 30
 cols = ["sub_name", "top_subs", "size"]
 subs = pd.DataFrame(columns=cols)
 
-
 # Iterate through subreddits
+# Each sub uses 1 + (3 * count) PRAW calls
 for sub in subreddits:
-    # Grab all authors
-
+    # These are at top to trigger PRAW api calls info
     subreddit = reddit.subreddit(sub)
+    subscribers = reddit.subreddit(sub).subscribers
+    # Make sure we have enough calls, otherwise wait
+    auth_limits = reddit.auth.limits
+    print(auth_limits)
+    # Keep track of how many calls each thing takes
+    if auth_limits and auth_limits["remaining"]:
+        left = auth_limits["remaining"]
+        # If we would run out of calls wait until reset
+        # This will need to be modified, currently 2 calls per author and 2
+        # per subreddit
+        if left - 2 - (2 * count) < 0:
+            timestamp = time.time()
+            reset_time = auth_limits["reset_timestamp"]
+            difference = reset_time - timestamp
+            # Sleep until we would reset
+            print(f"Waiting {int(difference)} seconds for more API calls!")
+            # Sleep if there is a difference
+            if (reset_time - timestamp > 0):
+                time.sleep(reset_time - timestamp)
+    elif count > 999:
+        print("Post limit is 999 per subreddit due ot API limits!")
+    # Grab all authors
     authors = []
     for submission in subreddit.top(time_filter = time_limit, limit=count):
         authors.append(submission.author)
     # Iterate through authors, getting their top subreddits and their karma
     top_subs = {}
     for author in authors:
+        # Feedback mechanism for large requests
+        print(author)
         if author != None:
             # Use praw to get the content of this user - avoids requests issue
             content = []
@@ -56,8 +82,9 @@ for sub in subreddits:
                     top_subs[item.subreddit] = item.score
     print(top_subs)
     sortable = dict(sorted(top_subs.items(), key=lambda item: item[1], reverse=True))
-    new_row = (sub, sortable, subreddit.subscribers)
+    new_row = (sub, sortable, subscribers)
     subs.loc[len(subs)] = new_row
+    print(str(reddit.auth.limits["remaining"]) + " calls left!")
 print(subs)
 # Save
 subs.to_csv('query.csv', index=False)
