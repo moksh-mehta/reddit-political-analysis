5977866810bf793e3ab081011a431e0c6f7c7b7b
gsb1776 <gray_bittker@brown.edu>
2025-03-13 21:38:01 -0400

    updated parameters

--- main.py
+++ main.py
@@ -9,21 +9,43 @@ import time
 import json
 import time
 from datetime import datetime
+from itertools import islice
 
 reddit = praw.Reddit(
     client_id="q33sziEQUMFF51P9IrtRHw",
     client_secret="xxNns4HmThugUzy6-o8LqsF8IY9W9A",
-    user_agent="userinfo",
+    user_agent="MacOS:q33sziEQUMFF51P9IrtRHw:1.0.3",
 )
 
-# List target subreddits
-subreddits = ["politics", "news", "socialism", "communism", "antiwork"]
+# Get target subreddits
+inputs = open("subs.txt", "r")
+data = inputs.read()
+subreddits = data.split("\n")
 subreddits = [item.lower() for item in subreddits]
 
-# Set some parameters
-time_limit = "month"
+# PARAMETERS
+# Which times to look for top posts
+times = ["year", "month", "week", "day"]
 # Number of posts to scan per subreddit
-count = 10
+# Impacts API call amount
+# Categories are top for year, month, week, day
+# THIS IS x4 as a result
+per_sub = 10
+# Number of items per category to get for each user
+# Does not impact API call amount but is multiplied by 4
+per_user = 50
+# Threshold for minimum member crossover to merit a deeper search
+threshold = 5
+# Max subreddit to search, limit time
+max_subs = 25
+# Minimum subscribers for a subreddit to be searched
+min_members = 100000
+# Max subscribers for a subreddit to be searched, limit irrelevant ones
+max_members = 100000000
+# Define a limit for how many sub-communities there should be
+max_sub_communities = 25
+# Define a limit for branching, i.e. only top 10
+branch_limit = 3
 
 # Subreddit info will be stored as a dataframe with:
 # top_subs, a dict with ["subreddit": karma]
@@ -31,12 +53,19 @@ count = 10
 cols = ["sub_name", "top_subs", "size"]
 subs = pd.DataFrame(columns=cols)
 
+# Output dict for json
+output = {}
+
 # Iterate through subreddits
-# Each sub uses 1 + (3 * count) PRAW calls
+# Each sub uses times + (times * per_sub * 4) calls
+# So year, month, week, day with 10 authors per sub is 164 calls per sub
 for sub in subreddits:
     # These are at top to trigger PRAW api calls info
     subreddit = reddit.subreddit(sub)
     subscribers = reddit.subreddit(sub).subscribers
+    # Skip if not enough subs
+    if subscribers < min_members or subscribers > max_members:
+        continue
     # Make sure we have enough calls, otherwise wait
     auth_limits = reddit.auth.limits
     print(auth_limits)
@@ -46,7 +75,7 @@ for sub in subreddits:
         # If we would run out of calls wait until reset
         # This will need to be modified, currently 4 calls per author and 2
         # per subreddit
-        if left - 2 - (4 * count) < 0:
+        if left - len(times) - (len(times) * 4 * per_sub) - 2 < 0:
             timestamp = time.time()
             reset_time = auth_limits["reset_timestamp"]
             difference = reset_time - timestamp
@@ -55,45 +84,68 @@ for sub in subreddits:
             # Sleep if there is a difference
             if (reset_time - timestamp > 0):
                 time.sleep(reset_time - timestamp)
-    elif count > 999:
+    elif per_sub > 999:
         print("Post limit is 999 per subreddit due ot API limits!")
     # Grab all authors
     authors = []
-    for submission in subreddit.top(time_filter = time_limit, limit=count):
-        authors.append(submission.author)
+    # Do year, month, week, day
+    for item in times:
+        for submission in subreddit.top(time_filter = item, limit=per_sub):
+            authors.append(submission.author)
     # Iterate through authors, getting their top subreddits and their karma
     top_subs = {}
-    for author in authors:
+    for author in set(authors):
+        print(author)
         # Keep track of their subreddits
         my_subs = []
         # Feedback mechanism for large requests
-        if author != None:
+        if author is not None:
             # Use praw to get the content of this user - avoids requests issue
             content = []
-            # DESIGN QUESTION: Do we want top or most recent posts/comments
-            # Add comments and posts
-            content.extend(reddit.redditor(author.name).comments.top(limit = 10))
-            content.extend(reddit.redditor(author.name).submissions.top(limit = 10))
-            content.extend(reddit.redditor(author.name).comments.new(limit = 10))
-            content.extend(reddit.redditor(author.name).submissions.new(limit = 10))
+            # Add comments and posts from both top and new
+            # Use try/except in case of a banned/private account
+            try:
+                content.extend(reddit.redditor(author.name).comments.top(limit = per_user))
+                content.extend(reddit.redditor(author.name).submissions.top(limit = per_user))
+                content.extend(reddit.redditor(author.name).comments.new(limit = per_user))
+                content.extend(reddit.redditor(author.name).submissions.new(limit = per_user))
+            except:
+                pass
             # Get top/newwest comments
-            for item in content:
-                # Add score
-                if item.subreddit in top_subs:
-                    top_subs[item.subreddit][0] += item.score
-                    if item.subreddit not in my_subs:
-                        top_subs[item.subreddit][1] += 1
-                        my_subs.append(item.subreddit)
-                else:
-                    top_subs[item.subreddit] = [item.score, 1]
-                    my_subs.append(item.subreddit)
-    print(top_subs)
+            for item in set(content):
+                # Get name of subreddit
+                display = item.subreddit.display_name
+                if display.lower().rstrip() != subreddit.display_name.lower().rstrip():
+                    # Add score
+                    if display in top_subs:
+                        top_subs[display][0] += item.score
+                        if display not in my_subs:
+                            # Add to member counter
+                            top_subs[display][1] += 1
+                            my_subs.append(display)
+                    else:
+                        top_subs[display] = [item.score, 1]
+                        my_subs.append(display)
+    time.sleep(3)
     # Fitler by shred subreddits
     new_top = {k: v for k, v in top_subs.items() if v[1] >= 2}
-    sortable = dict(sorted(new_top.items(), key=lambda item: item[1][0], reverse=True))
+    trimmed_top = dict(islice(new_top.items(), max_sub_communities))
+    sortable = dict(sorted(trimmed_top.items(), key=lambda item: item[1][0], reverse=True))
+    # Add crawling too, this uses BFS to go down layers
+    # Only get first branch_limit ones
+    count = 0
+    for k, v in sortable.items():
+        if count < branch_limit:
+            if v[1] >= threshold and k not in subreddits and max_subs > len(subreddits):
+                subreddits.append(k)
+                count += 1
+    print(subreddits)
+    output[sub] = (sortable, subscribers)
+    # Dump as json file
+    with open("output.json", 'w') as file:
+        json.dump(output, file, indent = 4)
     new_row = (sub, sortable, subscribers)
     subs.loc[len(subs)] = new_row
+    # Also save csv
+    subs.to_csv('output.csv', index=False)
     print(str(reddit.auth.limits["remaining"]) + " calls left!")
-print(subs)
-# Save
-subs.to_csv('query.csv', index=False)
